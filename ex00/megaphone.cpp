bc4a01ee6b70d0e2de49ae21ba8d2bbb20a6abff
swautelet <swautele@student.s19.be>
2022-07-05 16:53:37 +0200

    first

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <cctype>
+
+void	display_maj(char *str)
+{
+	std::string string0(str);
+	std::transform(string0.begin(), string0.end(), string0.begin(), ::toupper);
+	std::cout << string0;
+}
+
+int main(int argc, char **argv)
+{
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+		return (0);
+	}
+	for (int i = 1; i < argc; i++)
+	{
+		display_maj(argv[i]);
+	}
+    return 0;
+}
