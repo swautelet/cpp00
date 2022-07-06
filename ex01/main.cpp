0d29f39bccfc7acf41b9ad18234ece0356a3ec37
swautele <swautle@student.s19.com>
2022-07-06 23:28:13 +0200

    save GG

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -10,47 +10,36 @@ int	main()
 	std::string nickname;
 	std::string phone;
 	std::string darkestsecret;
-	char i;
+	std::string i;
 	Phonebook book;
 
 	while (str != "EXIT")
 	{
 		std::cout << "Enter ADD to add a contact SEARCH to search a contact or EXIT to exit and loose all your contact :" << std::endl;
-		std::cin >> str;
+		getline(std::cin, str);
 		if (str == "ADD")
 		{
 			std::cout << "Enter firstname :";
-			std::cin >> firstname;
+			getline(std::cin, firstname);
 			std::cout << "Enter lastname :";
-			std::cin >> lastname;
+			getline(std::cin, lastname);
 			std::cout << "Enter nickname :";
-			std::cin >> nickname;
+			getline(std::cin, nickname);
 			std::cout << "Enter phone :";
-			std::cin >> phone;
+			getline(std::cin, phone);
 			std::cout << "Enter darkest secret :";
-			std::cin >> darkestsecret;
+			getline(std::cin, darkestsecret);
 			book.addcontact(firstname, lastname, nickname, phone, darkestsecret);
 		}
 		else if (str == "SEARCH")
 		{
 			book.displaylist();
 			std::cout  << "Enter the numero of the contact  you want to see : ";
-			std::cin >> i;
-			switch (i)
-			{
-				case '1':
-				case '2':
-				case '3':
-				case '4':
-				case '5':
-				case '6':
-				case '7':
-				case '8':
-					book.searchcontact(i);
-				break ;
-				default:
-					std::cout << "Ivalid output" << std::endl;
-			}
+			getline(std::cin, i);
+			if (i.length() == 1)
+				book.searchcontact(i[0]);
+			else
+				std::cout << "Ivalid output" << std::endl;
 		}
 		else if (str != "EXIT")
 		{
