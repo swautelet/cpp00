aa4e3bfb39b3144c75fd92edffb82eafb945a72f
swautelet <swautele@student.s19.be>
2022-07-07 15:15:12 +0200

    save GG

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -17,7 +17,7 @@ Account::Account(int initial_deposit)
 Account::~Account(void)
 {
 	_displayTimestamp();
-	std::cout << " index:" << this->_accountIndex << ";amount" << this->_amount << ";closed" << std::endl;
+	std::cout << " index:" << this->_accountIndex << ";amount:" << this->_amount << ";closed" << std::endl;
 	Account::_nbAccounts--;
 	Account::_totalAmount -= this->_amount;
 }
