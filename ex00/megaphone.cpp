bba435f1c7b1b98351e2ad6b8bd1110b673ad6db
swautelet <swautele@student.s19.be>
2022-07-10 13:53:35 +0200

    save GG

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -7,14 +7,14 @@ void	display_maj(char *str)
 {
 	std::string string0(str);
 	std::transform(string0.begin(), string0.end(), string0.begin(), ::toupper);
-	std::cout << string0;
+	std::cout << string0 << " ";
 }
 
 int main(int argc, char **argv)
 {
 	if (argc == 1)
 	{
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return (0);
 	}
 	for (int i = 1; i < argc; i++)
